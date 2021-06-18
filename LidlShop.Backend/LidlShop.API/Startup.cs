eb87c2f458feaaaf654876092bbee0e7baa27a9b
Ludovic Baens <ludovic.baens@solidaris.be>
2021-06-18 22:39:44 +0200

    Ajout Swagger (nugget Swashbuckle.AspNetCore)

--- LidlShop.Backend/LidlShop.API/Startup.cs
+++ LidlShop.Backend/LidlShop.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,13 @@ namespace LidlShop.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                { Title = "LidlShop", Description = "DotNet Core Api 3 - with swagger" });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +54,13 @@ namespace LidlShop.API
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "LidlShop V1");
+            });
+
         }
     }
 }
